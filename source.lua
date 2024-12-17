feabec96cdeb8cd133e41b0f888b7c6443333fc8
iPigTW <ipig@ipig.me>
2024-12-17 19:35:17 +0800

    mercury on top

--- source.lua
+++ source.lua
@@ -1,31 +1,48 @@
-if (string.find(identifyexecutor(), "Seliware")) then
-    local coregui_ = game:GetService("CoreGui")
-    getgenv().gethui = function() return coregui_ end
-end
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "PigHub V2", HidePremium = false, SaveConfig = true, ConfigFolder = "PigHubV2"})
-local Universal = Window:MakeTab({
-	Name = "Universal",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local Mercury = loadstring(game:HttpGet(
+                               "https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create({
+    Name = "PigHub V2",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
 })
+local Universal =
+    GUI:Tab({Name = "Universal", Icon = "rbxassetid://4483345998"})
 
-Universal:AddButton({
-	Name = "UNC Test",
-	Callback = function()
-      		loadstring(game:HttpGet(("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/refs/heads/main/UNCCheckEnv.lua")))()
-  	end    
+Universal:Button({
+    Name = "UNC Test",
+    Callback = function()
+        loadstring(game:HttpGet(
+                       ("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/refs/heads/main/UNCCheckEnv.lua")))()
+    end
 })
-Universal:AddButton({
-	Name = "SUNC Test",
-	Callback = function()
-        loadstring(game:HttpGet("https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
-  	end    
+Universal:Button({
+    Name = "SUNC Test",
+    Callback = function()
+        loadstring(game:HttpGet(
+                       "https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
+    end
 })
-Universal:AddButton({
-	Name = "Chat Bypasser",
-	Callback = function()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua"))()
-  	end    
+Universal:Button({
+    Name = "Chat Bypasser",
+    Callback = function()
+        loadstring(game:HttpGetAsync(
+                       "https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua"))()
+    end
 })
-OrionLib:Init()
+Universal:Button({
+    Name = "Hydroxide",
+    Callback = function()
+        local owner = "Upbolt"
+        local branch = "revision"
+
+        local function webImport(file)
+            return loadstring(game:HttpGetAsync(
+                                  ("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(
+                                      owner, branch, file)), file .. '.lua')()
+        end
+
+        webImport("init")
+        webImport("ui/main")
+    end
+})

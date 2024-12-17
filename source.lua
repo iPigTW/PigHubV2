7f9d8bbae1b86af7281a390561f554ed4a016a3a
iPigTW <ipig@ipig.me>
2024-12-17 19:15:41 +0800

    oooo

--- source.lua
+++ source.lua
@@ -1,1 +1,28 @@
+if (string.find(identifyexecutor(), "Seliware")) then
+    local coregui_ = game:GetService("CoreGui")
+    getgenv().gethui = function()
+    return coregui_
+end
+end
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "PigHub V2", HidePremium = false, SaveConfig = true, ConfigFolder = "PigHubV2"})
+local Universal = Window:MakeTab({
+	Name = "Universal",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Universal:AddButton({
+	Name = "UNC Test",
+	Callback = function()
+      		loadstring(game:HttpGet(("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/refs/heads/main/UNCCheckEnv.lua")))()
+  	end    
+})
+Universal:AddButton({
+	Name = "SUNC Test",
+	Callback = function()
+        loadstring(game:HttpGet("https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
+  	end    
+})
+
+OrionLib:Init()
